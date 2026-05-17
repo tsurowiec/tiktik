1f625da0bd8cbaa465ed05eb8ddf8c983b456441
Tomasz Surowiec <tsurowiec@gmail.com>
2026-05-17 22:15:28 +0200

    [] SQLite WAL journal mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        DB::statement('PRAGMA journal_mode=WAL;');
     }
 
     /**
